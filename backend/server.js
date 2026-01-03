d156b35044c44dbbf5f35d53e2e41609b6da2f63
Soukarsha Dutta <dsoukarsha@gmail.com>
2026-01-03 17:01:59 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://contact-management-green.vercel.app/" 
+    "https://contact-management-green.vercel.app" 
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
